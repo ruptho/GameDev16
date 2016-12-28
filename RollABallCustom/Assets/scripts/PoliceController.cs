a501dfb55110bd84fcd0928081cb775f853b1401
Unknown <th.ruprechter@gmail.com>
2016-12-28 15:59:19 +0100

    further functionality for minimap/robber/policemen, massive refactoring
    WhatsappLog: 
    [15:39, 28.12.2016] +43 664 1519245: die minimap ist jetzt auf das fokusiert, was der robber sieht, das licht in der szene hab ich der einfachheithalber mal etwas aufgedreht, dann ists ersichtlicher was passiert                        
    [15:40, 28.12.2016] +43 664 1519245: auf der linken seite seht ihr ui anzeigen für den robber, auf der rechten für den policeman                        
    [15:41, 28.12.2016] +43 664 1519245: 1.) der robber kann sachen aufheben und fallen lassen (leertaste), und kann sachen ins ziel bringen (jetzt = 3 sachen, gewonnen)                        
    [15:43, 28.12.2016] +43 664 1519245: wenn er etwas aufgehoben hat, wird er langsamer                        
    [15:47, 28.12.2016] +43 664 1519245: 2) der policeman muss versuchen den robber zu erwischen - tut er das, gewinnt er das spiel - der robber lässt, falls er erwischt wird, das objekt dass er gerade hält fallen                        
    [15:47, 28.12.2016] +43 664 1519245: 3) der robber kann zurzeit maximal 1 objekt gleichzeitig halten. Ohne ein Objekt zu halten, bewegt er sich schneller als der policeman. Wenn er eines hält, ist er langsamer als die police                        
    [15:48, 28.12.2016] +43 664 1519245: 4) der policeman ist für den robber nur auf der minimap sichtbar, wenn er auch in seinem sichtfeld ist. wenn er nicht im sichtfeld ist weiß er nicht, wo sich sein gegner gerade befindet

---
+++ RollABallCustom/Assets/scripts/PoliceController.cs
@@ -0,0 +1,58 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoliceController : MonoBehaviour
+{
+    // variables defined in editor/inspector
+    public float speed;
+    public Text infoText; // UI element
+    public Text endText;  // UI element
+    public GameObject robber; // used to call functions for the robber player (e.g. signalLose)
+
+    // helper variables 
+    private Rigidbody rb; // the "real" rigidbody/sphere
+    private RobberController robberController; // the controller script for the robber
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        robberController = robber.GetComponent<RobberController>();
+        endText.text = "";
+    }
+
+    // before any physics calculation - put physics code here
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("VerticalPolice");
+        float moveHorizontal = Input.GetAxis("HorizontalPolice");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //rb.AddForce(movement * speed);
+        rb.velocity = movement * speed;
+    }
+
+    // collision detection with rigid bodies
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("robber"))
+        {
+            signalWin();
+            robberController.signalLose();
+        }
+    }
+
+    // this will be called by RobberController
+    public void signalLose()
+    {
+        infoText.text = "You let the robber steal the objects he wanted!";
+        endText.text = "You lose! :-(";
+    }
+
+    public void signalWin()
+    {
+        infoText.text = "You caught the robber!";
+        endText.text = "You win! :-)";
+    }
+}
