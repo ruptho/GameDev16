f6a4dce11c5e4c5809e31b093285737492f5d5b2
Nicole Weidinger <nicole.weidinger@student.tugraz.at>
2017-01-06 14:37:39 +0100

    Police model + animations

--- RollABallCustom/Assets/scripts/PoliceController.cs
+++ RollABallCustom/Assets/scripts/PoliceController.cs
@@ -14,10 +14,14 @@ public class PoliceController : MonoBehaviour
     private Rigidbody rb; // the "real" rigidbody/sphere
     private RobberController robberController; // the controller script for the robber
 
+    Animator anim;
+    Vector3 movement;
+
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
         robberController = robber.GetComponent<RobberController>();
         endText.text = "";
     }
@@ -28,9 +32,42 @@ public class PoliceController : MonoBehaviour
         float moveVertical = Input.GetAxis("VerticalPolice");
         float moveHorizontal = Input.GetAxis("HorizontalPolice");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        //rb.AddForce(movement * speed);
-        rb.velocity = movement * speed;
+      
+        // Move the player around the scene.
+        Move(moveHorizontal, moveVertical);
+
+        Animating(moveHorizontal, moveVertical);
+    }
+
+    void Move(float h, float v)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set(h, 0f, v);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.velocity = movement;
+        //--rotate in direction of movement
+        // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+        if (h != 0f || v != 0f)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(movement);
+
+            // Set the player's rotation to this new rotation.
+            rb.MoveRotation(newRotation);
+        }
+
+
+        // Move the player to it's current position plus the movement.
+        rb.MovePosition(transform.position + movement);
+    }
+
+    void Animating(float h, float v)
+    {
+        // Create a boolean that is true if either of the input axes is non-zero.
+        bool walking = h != 0f || v != 0f;
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool("IsWalking", walking);
     }
 
     // collision detection with rigid bodies
